05f8e276ce8708fae663ce5d048deecf91ac6ae0
leogenuit <genuitleo@gmail.com>
2022-11-14 17:10:02 +0100

    add update

--- routes/profil.routes.js
+++ routes/profil.routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model");
 
-router.get("/", (req, res, next) => {
-  res.render("profile/profile");
+router.get("/user/:id", async (req, res, next) => {
+  try {
+    const oneUser = await User.findById(req.params.id);
+    console.log(req.params.id);
+
+    res.redirect("profile/userProfile", { oneUser });
+  } catch (error) {
+    next(error);
+  }
 });
-
 module.exports = router;
