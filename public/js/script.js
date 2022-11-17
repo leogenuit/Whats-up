883504b5fc9944e0fc9a9ab599a079d55c8eef62
jeanne <jeanne.lefevre@hotmail.fr>
2022-11-17 13:01:13 +0100

    Unconnected users functionnalities

--- public/js/script.js
+++ public/js/script.js
@@ -6,64 +6,78 @@ document.addEventListener("DOMContentLoaded", () => {
 const allRooms = document.querySelector(".allsroom");
 const dedicatedRoom = document.querySelector(".dedicatedRoom");
 const messageTemplate = document.getElementById("message-template");
-const messagesContainer = document.querySelector("ul.messages");
+const messagesContainer = document.querySelector(".messages");
 
 let socket = io();
-let messages = document.querySelector("ul");
+//let messages = document.querySelector(".messages");
 let form = document.getElementById("form");
 let input = document.getElementById("input");
-let listUsers = document.querySelector(".allsroom");
+let listUsers = document.querySelector(".connected");
+let listUsersNotConnected = document.querySelector(".disconnected");
 
 const userId = form.dataset.id;
 
 socket.on("connect", () => {
   const sessionID = socket.id;
   socket.emit("add user", userId);
-  socket.emit("chatroom on", userId);
+  socket.emit("get user", userId);
+  //socket.emit("chatroom on", userId);
+  //socket.emit("get chatroom", userId);
 });
 
-socket.on("all users", (users) => {
+socket.on("all users", (users, notConnectUsers) => {
+  listUsers.innerHTML = "";
+  listUsersNotConnected.innerHTML = "";
   //console.log(users);
   users.forEach((element) => {
-    let a = document.createElement("a");
-    a.classList.add("room", "flex");
-    a.dataset.id = element.user._id;
-    a.textContent = element.user.username;
-    listUsers.appendChild(a);
-    a.addEventListener("click", function (e) {
+    let div = document.createElement("div");
+    div.classList.add("room", "flex");
+    div.dataset.id = element.user._id;
+    div.textContent = element.user.username;
+    listUsers.appendChild(div);
+
+    div.addEventListener("click", function (e) {
       e.preventDefault();
-      console.log(a.dataset.id);
-      socket.emit("chatroom on", a.dataset.id, userId);
-      socket.emit("get messages", userId, a.dataset.id);
+      socket.emit("chatroom on", div.dataset.id, userId);
+      socket.emit("get messages", userId, div.dataset.id);
     });
+
     form.addEventListener("submit", function (e) {
       e.preventDefault();
       if (input.value) {
-        socket.emit("chat message", input.value, userId, a.dataset.id);
+        socket.emit("chat message", input.value, userId, div.dataset.id);
         input.value = "";
       }
+    });
+  });
+
+  notConnectUsers.forEach((element) => {
+    let div = document.createElement("div");
+    div.classList.add("room", "flex", "grey");
+    div.dataset.id = element._id;
+    div.textContent = element.username;
+    listUsersNotConnected.appendChild(div);
+
+    div.addEventListener("click", function (e) {
+      e.preventDefault();
+      socket.emit("some room", div.dataset.id, userId);
+      socket.emit("get messages", div.dataset.id, userId);
     });
   });
 });
 
 socket.on("all messages", function (msg) {
   allRooms.classList.add("hidden");
+  listUsersNotConnected.classList.add("hidden");
   dedicatedRoom.classList.remove("hidden");
-
+  messagesContainer.innerHTML = "";
   msg.forEach((element) => {
     formatMessage(element);
-    // console.log(element.content);
-
-    // console.log(element.author.username);
   });
 });
 
 socket.on("chat message", function (msg) {
   formatMessage(msg);
-  // let item = document.createElement("li");
-  // item.textContent = msg;
-  // messages.appendChild(item);
-  // window.scrollTo(0, document.body.scrollHeight);
 });
 
 socket.on("delete user", (user) => {
